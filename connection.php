015b7cca2d26116009b1b75c10b0e2afeaacc09f
elaa <elaa.brahmii@gmail.com>
2024-08-03 11:37:30 +0100

    connectiondelete

--- connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-Lorem ipsum   amet consectetur adipisicing elit. Illo quia aliquam id beatae, corrupti asperiores obcaecati, perspiciatis eos numquam facilis quaerat tempora, dolorum quam ducimus qui commodi necessitatibus vitae voluptas.
-?>
