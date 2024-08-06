a1d20ffe75c9a01dca58d819deb85568ad759ebf
elaa <elaa.brahmii@gmail.com>
2024-08-06 12:03:33 +0100

    update login admin

--- php/loginControl.php
+++ php/loginControl.php
@@ -1,42 +1,72 @@
 <?php
 session_start();
 include "connection.php";
-$login=$_POST["login"];
+ $login=$_POST["login"];
 $pass=$_POST["password"];
-$req="select * from client where password=?";
-$req1="select * from admin where login=? and password=? ";
+$req="select * from user where login=? and password=?";
 try{
-    $stmt1=$pdo->prepare($req1);
-    $stmt1->bindparam(1,$login);
-    $stmt1->bindparam(2,$pass);
-    $stmt1->execute();
-    $row1=$stmt1->fetch();
-
-    $stmt=$pdo->prepare($req);
-    $stmt->bindparam(1,$pass);
-    $stmt->execute();
-    $row=$stmt->fetch();
-if (!$row && !$row1){
+$stmt=$pdo->prepare($req);
+$stmt->bindparam(1,$login);
+$stmt->bindparam(2,$pass);
+$stmt->execute();
+$row=$stmt->fetch();
+if(!$row){
     echo 'error';
-
 }
-else{
-    if(isset($row['idClient'])){
-
-        $_SESSION['id'] = $row['idClient'];
-    }
-    if(isset($row['password'])){
-        $_SESSION['password'] = $row['password'];
-    }
-    else{
-        $_SESSION['password'] = $row1['password']
-    }
-    $_SESSION["username"]=$login;
+else if($row["role"]=="admin"){
+    $_SESSION["login"]=$row["login"];
+    $_SESSION["id"]=$row["idUser"];
+    $_SESSION["role"]="admin";
     echo 'success';
 }
 }
 catch(PDOException $e){
-    echo 'error';
-}
+         echo 'error';
+    }
+
+
+
+
+
+
+
+// session_start();
+// include "connection.php";
+// $login=$_POST["login"];
+// $pass=$_POST["password"];
+// $req="select * from client where password=?";
+// $req1="select * from admin where login=? and password=? ";
+// try{
+//     $stmt1=$pdo->prepare($req1);
+//     $stmt1->bindparam(1,$login);
+//     $stmt1->bindparam(2,$pass);
+//     $stmt1->execute();
+//     $row1=$stmt1->fetch();
+
+//     $stmt=$pdo->prepare($req);
+//     $stmt->bindparam(1,$pass);
+//     $stmt->execute();
+//     $row=$stmt->fetch();
+// if (!$row && !$row1){
+//     echo 'error';
+// }
+// else{
+//     if(isset($row['idClient'])){
+
+//         $_SESSION['id'] = $row['idClient'];
+//     }
+//     if(isset($row['password'])){
+//         $_SESSION['password'] = $row['password'];
+//     }
+//     else{
+//         $_SESSION['password'] = $row1['password'];
+//     }
+//     $_SESSION["username"]=$login;
+//     echo 'success';
+// }
+// }
+// catch(PDOException $e){
+//     echo 'error';
+// }
 
 ?>
