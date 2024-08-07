7a1348579438af5e43b5baeac7acb1fe7f9430d8
elaa <elaa.brahmii@gmail.com>
2024-08-07 18:05:47 +0100

    update getProduct .php

--- php/productModule/getproduct.php
+++ php/productModule/getproduct.php
@@ -1,11 +1,11 @@
 <?php
-include "./connection.php";
+include "../connection.php";
 $req="select idProduct,name,description,price,picture,etat from products";
 try{
     $res=$pdo->query($req);
     $table=$res->fetchAll();
     $pdo=null;
-    header('Content-Type: application/json');
+  
     echo json_encode(['data' => $table]);
 
 
