9b49036b1d012a7bd712d67998f1c05487ed1f6b
elaa <elaa.brahmii@gmail.com>
2024-08-08 12:22:05 +0100

    update

--- index.php
+++ index.php
@@ -1,108 +1,153 @@
-<?php
-session_start();
-//require './php/authentification.php';
-?>
 <!doctype html>
+<?php
+include "php/authentification.php";
+//session_start();
+
+
+?>
 <html>
-    <head>
+<head>
+    <style>
+        .product-image-wrapper {
+            position: relative;
+            width: 180px;
+            height: 160px;
+            overflow: hidden;
+        }
+        .product-image {
+            width: 100%;
+            height: 100%;
+            transition: transform 0.3s ease;
+        }
+    </style>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="style.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body style="background-color: #eeebe8;">
-<nav style="border-radius:20px; width:700px; margin:auto; margin-top:25px;  background-color: #D8C3A5;">
+<nav style="border-radius:20px; width:700px; margin:auto; margin-top:25px; background-color: #D8C3A5;">
     <div class="nav-wrapper" style="font-weight:bold; font-size:35px; color:black;">
-      <a href="#" class="brand-logo"><img src="img/logo.png" style="width:85px; margin-top:-10px;"></a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down" >
-        <li><a href="">home</a></li>
-        <?php
-         if(isset($_SESSION["username"]) && $_SESSION["username"] =="ela"){
-           echo '<li><a href="clients.php">see clients</a></li>';}
-        ?>
-        <li><a href="about.html">about</a></li>
-        <li><a href="cart.php">cart</a></li>
-        <li><a href="profile.php">profile</a></li>
-      </ul>
+        <a href="#" class="brand-logo"><img src="img/logo.png" style="width:85px; margin-top:-10px;"></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="">home</a></li>
+            <?php
+            if(isset($_SESSION["role"]) && $_SESSION["role"] =="admin"){
+                echo '<li><a href="clients.php">see clients</a></li>';}
+            ?>
+            <li><a href="about.html">about</a></li>
+            <li><a href="cart.php">cart</a></li>
+            <li><a href="profile.php">profile</a></li>
+        </ul>
     </div>
-  </nav>
-   <div class="div1">
+</nav>
+<div id="cart"style="display:none;">
+    <button style="float:right; " onclick="closeCart()">X</button>
+
+
+
+
+
+
+</div>
+<div class="div1">
     <p>
         <h1>SHOP ALL</h1>
         Clean, vegan, cruelty-free skincare
     </p>
-    </div>
-    <div class="div2">
-        <table id="tabproduct" style="margin-top:40px;">
-            <thead>
-            <tr>
+</div>
+<div class="div2">
+    <h3 style="text-align:center;"> Best Sellers</h3>
+    <table id="tabproduct" style=" width:1000px; margin:auto; font-size:17px;">
+        <thead><tr>
             <th>id product</th>
-                <th>name</th>
-                <th>description</th>
-                <th>price</th>
-                <th>picture</th>
-                <th>state</th>
-            </tr>
-         </thead><tbody></tbody>
-</table>
-    </div>
-    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
-    <script>
-        $(document).ready(function(){
-            var table=$('#tabproduct').DataTable({
-                "ajax":{
-                    "url":"./php/productModule/getproduct.php",
-                    "type":"POST",
-                    "dataType":"json"
+            <th>name</th> <th>quantity</th>
+            <th>description</th>
+            <th>price</th>
+            <th>picture</th>
+            <th>state</th>
+        </tr></thead><tbody></tbody>
+    </table>
+</div>
+<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script>
+    function closeCart(){
+        var div1=document.getElementById("cart");
+        div1.style.display="none";
+    }
+    $(document).ready(function() {
+        var table = $('#tabproduct').DataTable({
+            "ajax": {
+                "url": "./php/productModule/getproduct.php",
+                "type": "POST",
+                "dataType": "json"
+            },
+            "columns": [
+                {"data": "idProduct"},
+                {"data": "name"},
+                {"data": "quantity"},
+                {"data": "description"},
+                {"data": "price"},
+                {
+                    "data": "picture",
+                    "render": function(data, type, row) {
+                        return "<div class='product-image-wrapper'><img src='img/" + data + "' class='product-image'></div>";
+                    }
                 },
-                "columns":[
-                    {"data":"idProduct"}
-                    {"date":"name"},
-                    {"date":"description"},
-                    {"date":"price"},
-                    {"date":"picture",
-                        "render": function(data, type, row) {
-                            return "<img src='"+data+"' width='50'>";  
-                        }
-                    },
-                    {"date":"etat",
-                        "render":function(data,type,row){
-                            if(data== "add to bag"){
-                                return "<button class='add-to-bag'>add to bag</button>";
-
-                            }
-                            else{
-                                return "coming soon;"
-                            }
+                {
+                    "data": "etat",
+                    "render": function(data, type, row) {
+                        if(data == "add to bag") {
+                            return "<button class='add-to-bag'>add to bag</button>";
+                        } else {
+                            return "coming soon";
                         }
                     }
-                ]
-            });
-            $('#tabproduct').on('click','.add-to-bag',function(){
-                var id=$(this).data('idProduct');
-                $.ajax({
-                    url:"./php/productModule/commander.php",
-                    type:"POST",
-                    data:{
-                        idProduct:id
-                    },
-                    sucess:function(response){
-                        alert("added successufully");
+                }
+            ]
+        });
 
-                    },
-                    error: function(xhr, status, error) {
-                                    console.log('AJAX Error: ' + error);
-                                }
-                });
+        $('#tabproduct').on('click', '.add-to-bag', function() {
+            var idProduct = $(this).closest('tr').find('td:first').text();
+            var quantity= $(this).closest('tr').find('td:eq(2)').text();
+            var price  = $(this).closest('tr').find('td:eq(4)').text();
+            console.log(id);
+            console.log(price);
+            console.log(quantity);
+            $.ajax({
+                url: "./php/commandeModule/addcommande.php",
+                type: "POST",
+                data: {
+                    idProduct: idProduct,
+                    price:price,
+                    quantity:quantity
+                },
+                success: function(response) {
+                    alert("added successfully");
+                },
+                error: function(xhr, status, error) {
+                    console.log('AJAX Error: ' + error);
+                }
             });
         });
-        </script>
+
+        $('#tabproduct').on('mouseover', '.product-image', function() {
+            $(this).css({
+                transform: 'scale(1.4)'
+            });
+        });
+
+        $('#tabproduct').on('mouseout', '.product-image', function() {
+            $(this).css({
+                transform: 'scale(1)'
+            });
+        });
+    });
+    $('#tabproduct').on('click', '.add-to-bag', function() {
+            console.log("button clicked");
+            var div1=document.getElementById("cart");
+            div1.style.display="block";});
+</script>
 </body>
 </html>
-
-
-
-
-
-
