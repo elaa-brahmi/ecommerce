4bc05d8a4098c986b893247cc27d3d93eace72a6
elaa <elaa.brahmii@gmail.com>
2024-08-14 00:35:00 +0100

    updaaaate

--- addClient.php
+++ addClient.php
@@ -3,7 +3,6 @@
     <head>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
 </head>
         <body>
             <form method="POST" id="clientform" class="container" style="margin-top:50px;">
