1f3f20233c009a894bef460fb076f62e13db77e5
elaa <elaa.brahmii@gmail.com>
2024-08-13 17:41:56 +0100

    update

---
+++ php/clientModule/getClientbyid.php
@@ -0,0 +1,14 @@
+<?php
+include "../connection.php";
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+$sql="select * from client where idClient=?";
+$stmt=$pdo->prepare($sql);
+$stmt->bindparam(1,$id);
+$stmt->execute();
+$client=$stmt->fetch();
+if($client){
+    echo json_encode($client);
+}}
+?>
