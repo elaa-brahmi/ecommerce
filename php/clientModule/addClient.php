0503ec41772f951c531cb8770c757ed68017b520
elaa <elaa.brahmii@gmail.com>
2024-08-14 10:43:08 +0100

    update

--- php/clientModule/addClient.php
+++ php/clientModule/addClient.php
@@ -1,7 +1,7 @@
 <?php
 include "../connection.php";
 try{
-
+    $pdo->beginTransaction();
 $name = $_POST["name"];
 $lname = $_POST["lastName"];
 $phone = $_POST["phone"];
@@ -18,7 +18,16 @@ $stmt->bindParam(4, $phone);
 $stmt->bindParam(5, $adress);
 $stmt->bindParam(6, $hashedPassword);
 $stmt->execute();
-echo 'hfhfhhh';
+$idClient = $pdo->lastInsertId();
+$reqUser = "INSERT INTO user (login, password, id, role) VALUES (?, ?, ?, 'client')";
+    $stmtUser = $pdo->prepare($reqUser);
+    $stmtUser->bindParam(1, $name);
+    $stmtUser->bindParam(2, $hashedPassword);
+    $stmtUser->bindParam(3, $idClient);
+    $stmtUser->execute();
+
+    // Commit the transaction
+    $pdo->commit();
 }
 catch(PDOException $e){
     echo $sql . "<br>" . $e->getMessage();
