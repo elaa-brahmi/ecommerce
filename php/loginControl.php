0f57bdcc1cebfebaa8ffe1174eed56c0a0df419a
elaa <elaa.brahmii@gmail.com>
2024-08-16 10:15:07 +0100

    update

--- php/loginControl.php
+++ php/loginControl.php
@@ -1,33 +1,45 @@
 <?php
 session_start();
 include "connection.php";
-$login=$_POST["login"];
-$pass=$_POST["password"];
-$req="select * from user where login=? and password=?";
-try{
-    $stmt=$pdo->prepare($req);
-    $stmt->bindparam(1,$login);
-    $stmt->bindparam(2,$pass);
+
+$login = $_POST["login"];
+$pass = $_POST["password"];
+$req = "SELECT * FROM user WHERE login = ?";
+
+try {
+    $stmt = $pdo->prepare($req);
+    $stmt->bindParam(1, $login);
     $stmt->execute();
-    $row=$stmt->fetch();
-    if(!$row){
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if (!$row || !password_verify($pass, $row['password'])) {
         echo 'error';
-    }
-    else {
-        if($row["role"]=="admin"){
-            $_SESSION["login"]=$row["login"];
-            $_SESSION["id"]=$row["idUser"];
-            $_SESSION["role"]="admin";
-            echo 'success_admin'; // the response
+    } else {
+        if ($row["role"] == "admin") {
+            $_SESSION["login"] = $row["login"];
+            $_SESSION["id"] = $row["idUser"];
+            $_SESSION["role"] = "admin";
+            echo 'success_admin';
+        } else {
+            $req1 = "select c.idClient 
+                     from client c 
+                     join user u on c.idClient = u.id
+                     where u.idUser = ?";
+            $stmt1 = $pdo->prepare($req1);
+            $stmt1->bindParam(1, $row["idUser"]);
+            $stmt1->execute();
+            $res = $stmt1->fetch(PDO::FETCH_ASSOC);
+
+            if ($res) {
+                $_SESSION["id"] = $res["idClient"];
+                $_SESSION["role"] = "client";
+                echo 'success_client';
+            } else {
+                echo 'error';
+            }
         }
-        else{
-            $_SESSION["id"]=$row["idUser"];
-            $_SESSION["role"]="client";
-            echo 'success_client'; // this is the response
-        }
     }
+} catch (PDOException $e) {
+    echo 'error: ' . $e->getMessage(); 
 }
-catch(PDOException $e){
-         echo 'error';
-    }
 ?>
