9ad308fa482be01990a140814816fdaebfc10d3d
elaa <elaa.brahmii@gmail.com>
2024-08-19 12:34:20 +0100

    update

--- profile.php
+++ profile.php
@@ -1,5 +1,44 @@
+<!doctype html>
 <?php
 session_start();
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css?<? time() ?>">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+<body style="background-color: #eeebe8;">
+<nav style="border-radius:20px; width:700px; margin:auto; color:black; margin-top:25px; background-color: #D8C3A5;">
+    <div class="nav-wrapper" style="font-weight:bold; font-size:35px; color:black;">
+        <a href="#" class="brand-logo"><img src="img/logo.png" style="width:85px; margin-top:-10px;"></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="index.php" style="color:black !important; text-decoration:none;">Home</a></li>
+            <li><a href="about.php" style="color:black !important; text-decoration:none;">About</a></li>
+            <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") { ?>
+                <li><a href="clients.php" style="color:black !important; text-decoration:none;">See Clients</a></li>
+                <li><a href="seeCommands.php" style="color:black !important; text-decoration:none;">See list of commands </a></li>
+            <?php } ?>
+            <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "client") { ?>
+                <li><a href="profile.php" style="color:black !important; text-decoration:none;">Profile</a></li>
+               
+            <?php } ?>
+            <?php if (!isset($_SESSION["role"]) || $_SESSION["role"] != "admin") { ?>
+                <li><a href="panier.php" style="color:black !important; text-decoration:none;">Cart</a></li>
+            <?php } ?>
+
+            <?php if (isset($_SESSION["role"])) { ?>
+                <li><a href="logOut.php" style="color:black !important; text-decoration:none;">Log Out</a></li>
+            <?php } ?>
+        </ul>    
+    </div>
+    <?php if (!isset($_SESSION["role"])) { ?>
+        <a href="login.php"><img src="img/login-avatar.png" style="width:30px; height:30px; position:absolute; top:40px; right:30px;"></a>
+    <?php } ?>
+</nav> 
+ <?php
+
 if(isset($_SESSION["role"])){
     $idClient=$_SESSION["id"];
 }
@@ -41,7 +80,7 @@ if(isset($_SESSION["role"])){
             </div>
         </div>
     </form>
-    <a href="index.php"  class="btn btn-primary" >home </a>
+   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
    
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
